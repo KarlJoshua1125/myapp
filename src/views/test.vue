b42690be8b29b310ed79b9f544339d5ee2b54a8f
Karl Joshua Gonzales <karl.joshuagonzales25@gmail.com>
2022-10-18 22:13:27 +0800

    third commit

--- src/views/test.vue
+++ src/views/test.vue
@@ -6,14 +6,34 @@
     <v-card-title>
       Location:
     </v-card-title>
-    <p>Latitude: {{covidData.data.observer.location.latitude}}</p>
-    <p>Longitude: {{covidData.data.observer.location.longitude}}</p>
-    <p>Elevation: {{covidData.data.observer.location.elevation}}</p>
-    <v-card-title>
+    
+  
+   
+
+    <v-list-item
+          v-for="astro in covidData"
+          :key="astro.data"
+        >
+
+        <v-list-item>
+          <p>Latitude: {{astro.observer.location.latitude}}</p>
+          <p>Longitude: {{astro.observer.location.longitude}}</p>
+          
+
+        </v-list-item>
+        <v-card-title>
       Dates:
     </v-card-title>
-    <p>From: {{covidData}}</p>
-    <p>To: {{covidData}}</p>
+
+        <v-list-item>
+          <p>From: {{astro.dates.from}}</p>
+          <p>To: {{astro.dates.to}}</p>
+          
+
+        </v-list-item>
+          
+          
+    </v-list-item>
    
    
    
@@ -46,7 +66,7 @@ async function getCovidData() {
       'X-RapidAPI-Key': '60f3c344f0msh2d09e6fd40809d5p100669jsn01a027b62d38',
       'X-RapidAPI-Host': 'astronomy.p.rapidapi.com',
     },
-  }).then((response) => {
+  }).then(response => {
     covidData.value = response.data;
     console.log(covidData.value);
   });
